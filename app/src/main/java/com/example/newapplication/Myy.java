f43c596d1f91214d7cefcd33112ab4a90a704363
liutianyuan <13957851292qq.com>
2020-10-21 18:35:51 +0800

    大作业

--- app/src/main/java/com/example/newapplication/Myy.java
+++ app/src/main/java/com/example/newapplication/Myy.java
@@ -2,13 +2,29 @@ package com.example.newapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class Myy extends AppCompatActivity {
+public class Myy extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myy);
+        findViewById(R.id.nodeng).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.nodeng:
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                break;
+        }
+
     }
 }
