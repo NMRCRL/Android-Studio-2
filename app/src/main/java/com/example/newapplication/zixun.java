370579e1515c468cd243e34ea70bf5ec0c89d03d
liutianyuan <13957851292qq.com>
2020-10-22 11:40:36 +0800

    大作业2

--- app/src/main/java/com/example/newapplication/zixun.java
+++ app/src/main/java/com/example/newapplication/zixun.java
@@ -2,13 +2,28 @@ package com.example.newapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class zixun extends AppCompatActivity {
+public class zixun extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zixun);
+        findViewById(R.id.imageView).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imageView:
+                Intent intent = new Intent(this, zixuntiao.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                break;
+        }
+
     }
 }
