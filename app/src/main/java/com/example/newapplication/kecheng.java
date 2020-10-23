803977beeaffca714c23f09c95df01cd0aebd8a8
liutianyuan <13957851292qq.com>
2020-10-23 12:04:15 +0800

    大作业5

--- app/src/main/java/com/example/newapplication/kecheng.java
+++ app/src/main/java/com/example/newapplication/kecheng.java
@@ -2,15 +2,36 @@ package com.example.newapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class kecheng extends AppCompatActivity {
+public class kecheng extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kecheng);
+        findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.button1).setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.button:
+                Intent intent = new Intent(this, kehchengtiao.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                break;
+            case R.id.button1:
+                 intent = new Intent(this, kechengtiao1.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                break;
+        }
 
     }
 }
